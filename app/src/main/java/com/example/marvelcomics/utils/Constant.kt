e0017388755a17d9267f0985e4699d94c3e0615f
Ammar Thaar <amar.at909@gmail.com>
2021-12-01 01:01:37 +0300

    fix: add constant and binding adapter

--- app/src/main/java/com/example/marvelcomics/utils/Constant.kt
+++ app/src/main/java/com/example/marvelcomics/utils/Constant.kt
@@ -1,4 +1,13 @@
 package com.example.marvelcomics.utils
 
 object Constant {
+    const val BASE_URL = "https://gateway.marvel.com/v1/public/"
+
+    const val BASE_IMAGE_URL = "https://i.annihil.us/u/prod/marvel/i/mg/"
+
+
+    const val PUBLIC_API_KEY = "6fa3629429e73031d27b0842ae4bc42a"
+    const val PRIVATE_API_KEY="b39438ca0c9a07c9aa563f0d298f17027d3dbe62"
+
+
 }
--- app/src/main/java/com/example/marvelcomics/utils/bindingAdapters.kt
+++ app/src/main/java/com/example/marvelcomics/utils/bindingAdapters.kt
@@ -1,2 +1,44 @@
 package com.example.marvelcomics.utils
 
+import android.view.View
+import androidx.core.view.isVisible
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.marvelcomics.R
+import com.example.marvelcomics.model.State
+import com.example.marvelcomics.ui.base.BaseAdapter
+import com.google.android.material.imageview.ShapeableImageView
+
+@BindingAdapter(value = ["app:showWhenLoading"])
+fun <T> showWhenLoading(view: View, state: State<T>?) {
+    view.isVisible = (state is State.Loading)
+}
+
+@BindingAdapter(value = ["app:showWhenError"])
+fun <T> showWhenError(view: View, state: State<T>?) {
+    view.isVisible = (state is State.Error)
+}
+
+@BindingAdapter(value = ["app:showWhenSuccess"])
+fun <T> showWhenSuccess(view: View, state: State<T>?) {
+    view.isVisible = (state is State.Success)
+}
+
+@BindingAdapter(value = ["app:items"])
+fun <T> setRecyclerItems(view: RecyclerView?, items: List<T>?) {
+    (view?.adapter as BaseAdapter<T>?)?.setItems(items ?: emptyList())
+}
+
+@BindingAdapter(value = ["app:image"])
+fun setImage(view: ShapeableImageView?, imagePath: String?) {
+    view?.let {
+        Glide.with(view)
+            .load(Constant.BASE_IMAGE_URL + imagePath)
+            .placeholder(R.drawable.ic_baseline_downloading_24)
+            .centerCrop()
+            .error(R.drawable.ic_baseline_error_outline_24)
+            .centerCrop()
+            .into(view)
+    }
+}
