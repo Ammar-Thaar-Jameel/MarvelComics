6865303cb9e806c9fdb62ccc505756e34e99dfff
Ammar Thaar <amar.at909@gmail.com>
2021-12-01 12:49:23 +0300

    fix: add internet permission

--- app/src/main/java/com/example/marvelcomics/MainViewModel.kt
+++ app/src/main/java/com/example/marvelcomics/MainViewModel.kt
@@ -2,11 +2,13 @@ package com.example.marvelcomics
 
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.marvelcomics.domain.repository.MarvelRepository
 import com.example.marvelcomics.domain.repository.MarvelRepositoryImpl
 import com.example.marvelcomics.model.State
 import com.example.marvelcomics.model.response.CharactersDto
 import com.example.marvelcomics.ui.base.BaseViewModel
+import kotlinx.coroutines.launch
 
 class MainViewModel:BaseViewModel() {
 
@@ -16,11 +18,11 @@ class MainViewModel:BaseViewModel() {
            // getChar()
     }
     fun getChar(){
-        collectValue(repository.getCharacters(),character)
+      // collectValue(repository.getCharacters(),character)
         Log.i("ooo","this..toString()")
-//        viewModelScope.launch {
-//           val x= repository.getCharacters()
-//            Log.i("sss",x.toString())
-//        }
+        viewModelScope.launch {
+           val x= repository.getCharacters()
+            Log.i("sss",x.toString())
+        }
     }
 }
